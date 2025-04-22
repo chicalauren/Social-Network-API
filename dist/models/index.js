5855ce43503d1c2ddaebe35b33df21cda1898804
Lauren Lambert <lambertelauren@gmail.com>
2025-04-22 18:46:39 -0500

    debug reset

--- dist/models/index.js
+++
@@ -1,3 +0,0 @@
-import User from "./user.js";
-import Thought from "./thought.js";
-export { User, Thought };
--- dist/routes/api/index.mjs
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { userRoute } from './userRoute.js';
-import { thoughtRoute } from './thoughtRoute.js';
-const router = Router();
-router.use('/users', userRoute);
-router.use('/thought', thoughtRoute);
-export default router;
